065236fa6ba08906e63d9ffe40664a1115f2757f
unknown <pkpkpk3938@gmail.com>
2026-09-18 12:01:28 +0530

    initial commit

---
+++ cart_example_oops.py
@@ -0,0 +1,28 @@
+class Cart:
+
+#       products=[]
+#       def __init__(self,name,price,quantity):
+#        self.product_name=name
+#        self.price=price
+#        self.quantity=quantity
+
+#       def add_products(self):
+#             product={}
+#             product["Product_name"]=self.product_name
+#             product["price"]=self.price
+#             product["quantity"]=self.quantity
+#             Cart.products.append(product)
+
+#       def display_products(self):
+#           print(self.products)
+
+#       def calculatetotal(self):
+#           total=self.quantity*self.price
+#           if total>1000:
+#               total=total*10/100
+#           print(f"Total:{total}")
+
+# shop1=Cart("maggie",500054,5)
+# shop1.add_products()
+# shop1.display_products()
+# shop1.calculatetotal()
